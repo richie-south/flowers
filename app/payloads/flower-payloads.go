ae414e0c6796c16502ac150284a926ed45175e14
richie-south <richard.soderman@gmail.com>
2017-09-22 20:46:16 +0200

    feat: added more props to flower

--- app/payloads/flower-payloads.go
+++ app/payloads/flower-payloads.go
@@ -2,28 +2,37 @@ package payloads
 
 import (
 	"net/http"
+	"time"
 
 	"gopkg.in/mgo.v2/bson"
 )
 
-type WaterTimeline struct {
-	Timestamp string `json:"timestamp"`
-	Amount    string `json:"amount"`
+type WaterTimelineItem struct {
+	Timestamp time.Time `json:"timestamp"`
+	Amount    string    `json:"amount"`
+}
+
+type RecivedWatering struct {
+	Amount string `json:"amount"`
 }
 
 // Flower type
 type Flower struct {
-	ID               bson.ObjectId   `json:"id" bson:"_id,omitempty"`
-	Name             string          `json:"name"`
-	WateringTimeline []WaterTimeline `json:"waterTimeline"`
+	ID                       bson.ObjectId       `json:"id" bson:"_id,omitempty"`
+	Name                     string              `json:"name"`
+	FlowerType               string              `json:"flowerType"`
+	OptimalWateringIntervall int                 `json:"optimalWateringIntervall"`
+	WateringTimeline         []WaterTimelineItem `json:"waterTimeline" bson:"waterTimeline"`
 }
 
 // RecivedFlower valid data server can recive
 type RecivedFlower struct {
-	Name string `json:"name"`
+	Name                     string `json:"name"`
+	FlowerType               string `json:"flowerType"`
+	OptimalWateringIntervall int    `json:"optimalWateringIntervall"`
 }
 
-func (fl *Flower) Render(w http.ResponseWriter, r *http.Request) error {
+func (flower *Flower) Render(w http.ResponseWriter, r *http.Request) error {
 	// Pre-processing
 	/*
 	 * metrics for:
@@ -36,8 +45,8 @@ func (fl *Flower) Render(w http.ResponseWriter, r *http.Request) error {
 	 *
 	 */
 	// default values
-	if fl.WateringTimeline == nil {
-		fl.WateringTimeline = []WaterTimeline{}
+	if flower.WateringTimeline == nil {
+		flower.WateringTimeline = []WaterTimelineItem{}
 	}
 
 	return nil
