47d2ef312432742d5fa66b1d32d2a6b280842289
richie-south <richard.soderman@gmail.com>
2017-09-22 20:47:58 +0200

    feat: water flower and recive new flower object, adds water timeline item to flower in db

---
+++ app/handlers/watering.go
@@ -0,0 +1,77 @@
+package handlers
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/render"
+	"github.com/richie-south/flowers/app/services"
+
+	"gopkg.in/mgo.v2/bson"
+
+	"gopkg.in/mgo.v2"
+
+	"github.com/richie-south/flowers/app/payloads"
+)
+
+func WaterFlower(w http.ResponseWriter, req *http.Request) {
+	flowerID := chi.URLParam(req, "flowerID")
+	decoder := json.NewDecoder(req.Body)
+	waterAmout := payloads.RecivedWatering{}
+	err := decoder.Decode(&waterAmout)
+	if err != nil {
+		render.Render(w, req, payloads.ErrUnexpected)
+	}
+
+	defer req.Body.Close()
+
+	waterTimelineItem := &payloads.WaterTimelineItem{
+		Timestamp: time.Now(),
+		Amount:    waterAmout.Amount,
+	}
+
+	err = dbAddToWaterTimeline(flowerID, *waterTimelineItem)
+	if err != nil {
+		render.Render(w, req, payloads.ErrWithDatabase)
+		return
+	}
+
+	flower, err := dbGetFlower(flowerID)
+	if err != nil {
+		render.Render(w, req, payloads.ErrWithDatabase)
+		return
+	}
+
+	render.Render(w, req, &flower)
+}
+
+func dbAddToWaterTimeline(id string, waterTime payloads.WaterTimelineItem) error {
+	query := func(collection *mgo.Collection) error {
+		if bson.IsObjectIdHex(id) {
+			err := collection.Update(
+				bson.M{"_id": bson.ObjectIdHex(id)},
+				bson.M{"$push": bson.M{"waterTimeline": waterTime}},
+			)
+
+			if err != nil {
+				fmt.Println("error", err)
+				return err
+			}
+
+			return nil
+		}
+
+		return errors.New("Id not object hex")
+	}
+
+	err := services.WithCollection("flower", query)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
