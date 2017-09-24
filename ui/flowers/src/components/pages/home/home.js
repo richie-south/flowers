ed2cafcd9cb4b364a3870d85988818d6ea5c9a18
richie-south <richard.soderman@gmail.com>
2017-09-24 21:43:33 +0200

    feat: render basic toolbox layout

--- ui/flowers/src/components/pages/home/home.js
+++ ui/flowers/src/components/pages/home/home.js
@@ -1,16 +1,21 @@
 import * as React from 'react'
 import {compose, withHandlers, withState, defaultProps} from 'recompose'
+import Layout from 'react-toolbox/lib/layout/Layout'
+import Panel from 'react-toolbox/lib/layout/Panel'
+import AppBar from 'react-toolbox/lib/app_bar/AppBar'
+import {ListFlowers} from '../../ui/list-flowers'
 
 const enhance = compose(
-  defaultProps({
-    name: 'Test',
-  })
 )
 
-export const StatelessHome = ({name}) => (
-  <div>
-    <h1>HOME {name}</h1>
-  </div>
+export const StatelessHome = ({}) => (
+  <Layout>
+    <Panel>
+      <AppBar title="Flowers" />
+      <h1>HOME</h1>
+      <ListFlowers />
+    </Panel>
+  </Layout>
 )
 
 export const Home = enhance(StatelessHome)
