87ec87f154178b1597dc38acea686cc7283b0e91
richie-south <richard.soderman@gmail.com>
2017-09-26 21:57:57 +0200

    feat: flower api urls

--- ui/flowers/src/config/urls.js
+++ ui/flowers/src/config/urls.js
@@ -1,3 +1,4 @@
 export const API_URL = 'http://localhost:3333/api'
-
 export const GET_FLOWER_LIST_URL = `${API_URL}/flowers`
+export const GET_FLOWER_URL = id => `${API_URL}/flowers/${id}`
+export const WATER_FLOWER_URL = id => `${API_URL}/flowers/${id}/water`
