827a9a1fa586727d4a6f6dc9c2f40f54b70f6f8e
richie-south <richard.soderman@gmail.com>
2017-09-28 21:16:38 +0200

    refactor: water intervall

--- server/app/payloads/flower-payloads.go
+++ server/app/payloads/flower-payloads.go
@@ -12,18 +12,24 @@ type WaterTimelineItem struct {
 	Amount    string    `json:"amount"`
 }
 
+type WaterIntervall struct {
+	Optimal     float64 `json:"optimal"`
+	OptimalText string  `json:"optimalText" bson:"optimalText"`
+	CurrentText string  `json:"currentText" bson:"currentText"`
+}
+
 type RecivedWatering struct {
 	Amount string `json:"amount"`
 }
 
 // Flower type
 type Flower struct {
-	ID                       bson.ObjectId       `json:"id" bson:"_id,omitempty"`
-	Name                     string              `json:"name"`
-	FlowerType               string              `json:"flowerType" bson:"flowerType"`
-	OptimalWateringIntervall float64             `json:"optimalWateringIntervall" bson:"optimalWateringIntervall"`
-	WateringTimeline         []WaterTimelineItem `json:"waterTimeline" bson:"waterTimeline"`
-	NextWateringSession      time.Time           `json:"nextWateringSession" bson:"nextWateringSession"`
+	ID                  bson.ObjectId       `json:"id" bson:"_id,omitempty"`
+	Name                string              `json:"name"`
+	FlowerType          string              `json:"flowerType" bson:"flowerType"`
+	WaterIntervall      WaterIntervall      `json:"waterIntervall" bson:"waterIntervall"`
+	WateringTimeline    []WaterTimelineItem `json:"waterTimeline" bson:"waterTimeline"`
+	NextWateringSession time.Time           `json:"nextWateringSession" bson:"nextWateringSession"`
 }
 
 // RecivedFlower valid data server can recive
@@ -46,6 +52,7 @@ func (flower *Flower) Render(w http.ResponseWriter, r *http.Request) error {
 	 *
 	 */
 	// default values
+	// check if NextWateringSession date has passed if has passed set date to now
 
 	if flower.WateringTimeline == nil {
 		flower.WateringTimeline = []WaterTimelineItem{}
