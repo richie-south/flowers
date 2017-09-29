862b5938bcd027a57d2db1b29dca0c7f2d515715
richie-south <richard.soderman@gmail.com>
2017-09-29 19:53:18 +0200

    refactor: using d, h instead of days, hours. remade currentText to type intervall

--- server/app/payloads/water-intervall.go
+++ server/app/payloads/water-intervall.go
@@ -11,23 +11,15 @@ type Intervall struct {
 type WaterIntervall struct {
 	Optimal Intervall `json:"optimal"`
 	/* 	OptimalText string    `json:"optimalText" bson:"optimalText"` */
-	CurrentText string `json:"currentText" bson:"currentText"`
+	Current Intervall `json:"current"`
 }
 
 func (intervall *Intervall) ToText() string {
 	var dayString string
 	var hourString string
 
-	if intervall.Days > 1 {
-		dayString = fmt.Sprintf("%v days", intervall.Days)
-	} else {
-		dayString = fmt.Sprintf("%v day", intervall.Days)
-	}
-	if intervall.Hours > 1 {
-		hourString = fmt.Sprintf("%v hours", intervall.Hours)
-	} else {
-		hourString = fmt.Sprintf("%v hour", intervall.Hours)
-	}
+	dayString = fmt.Sprintf("%vd", intervall.Days)
+	hourString = fmt.Sprintf("%vh", intervall.Hours)
 
 	intervallText := fmt.Sprintf("%v %v", dayString, hourString)
 	intervall.Text = intervallText
